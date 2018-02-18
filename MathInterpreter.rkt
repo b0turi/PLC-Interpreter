2e73d41012af02c972491d432c987bf1d570b857
Alex Hemm <Batman@Alexs-MacBook-Pro-7.local>
2018-02-18 15:32:15 -0500

    not finished, but what I have so far

---
+++ MathInterpreter.rkt
@@ -0,0 +1,24 @@
+(require "simpleParser.scm")
+
+; Main interpreter function
+
+(define interpret
+  (lambda (parser "testfile.txt")
+    ))
+
+
+; Mathematical Operators
+
+
+
+
+; M-state operations
+
+
+
+; M-value operations
+
+
+
+; M-boolean operations
+
