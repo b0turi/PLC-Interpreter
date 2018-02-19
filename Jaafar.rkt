7e2d072ae33303d5b7dccf450b1f8e59db210cb1
Alex Hemm <Batman@Alexs-MacBook-Pro-7.local>
2018-02-18 22:15:58 -0500

    Alex final push

--- Jaafar.rkt
+++ Jaafar.rkt
@@ -20,7 +20,7 @@
       ((eq? (operator stmt) 'return) (M_assign 'return (M_value (return stmt) s) s))
       ((eq? (operator stmt) 'var) (add (var-name stmt) (M_value (assignment stmt) (M_state (assignment stmt) s))))
       ((eq? (operator stmt) 'while) (M_state_while (condition stmt) (body stmt) s))
-      (else s)))))
+      (else s))))
 
 
 ; M_value
@@ -36,7 +36,21 @@
 (define M_evaluate
   (lambda (exp s)
     (cond
-      ((eq? (opperator stmt) '+) (+ (M_value (oprand1 exp) s) (M_value (oprand2 exp) (M_state (oprand1 exp) s))))
+      ((eq? (operator stmt) '+) (M_value (+ (M_value (operand1 exp) s) (M_value (operand2 exp) (M_state (operand1 exp) s)))))
+      ((eq? (operator stmt) '-) (M_value (- (M_value (operand1 exp) s) (M_value (operand2 exp) (M_state (operand1 exp) s)))))
+      ((eq? (operator stmt) '*) (M_value (* (M_value (operand1 exp) s) (M_value (operand2 exp) (M_state (operand1 exp) s)))))
+      ((eq? (operator stmt) '/) (M_value (quotient (M_value (operand1 exp) s) (M_value (operand2 exp) (M_state (operand1 exp) s)))))
+      ((eq? (operator stmt) '%) (M_value (remainder (M_value (operand1 exp) s) (M_value (operand2 exp) (M_state (operand1 exp) s)))))
+      ((eq? (operator stmt) '==) (M_boolean (= (M_boolean (operand1 exp) s) (M_boolean (operand2 exp) (M_state (operand1 exp) s)))))
+      ((eq? (operator stmt) '!=) (M_boolean (not (M_boolean (operand1 exp) s) (M_boolean (operand2 exp) (M_state (operand1 exp) s)))))
+      ((eq? (operator stmt) '<) (M_boolean (< (M_boolean (operand1 exp) s) (M_boolean (operand2 exp) (M_state (operand1 exp) s)))))
+      ((eq? (operator stmt) '>) (M_boolean (> (M_boolean (operand1 exp) s) (M_boolean (operand2 exp) (M_state (operand1 exp) s)))))
+      ((eq? (operator stmt) '<=) (M_boolean (<= (M_boolean (operand1 exp) s) (M_boolean (operand2 exp) (M_state (operand1 exp) s)))))
+      ((eq? (operator stmt) '>=) (M_boolean (>= (M_boolean (operand1 exp) s) (M_boolean (operand2 exp) (M_state (operand1 exp) s)))))
+      ((eq? (operator stmt) '>=) (M_boolean (>= (M_boolean (operand1 exp) s) (M_boolean (operand2 exp) (M_state (operand1 exp) s)))))
+      ((eq? (operator stmt) '&&) (M_boolean (and (M_boolean (operand1 exp) s) (M_boolean (operand2 exp) (M_state (operand1 exp) s)))))
+      ((eq? (operator stmt) '||) (M_boolean (or (M_boolean (operand1 exp) s) (M_boolean (operand2 exp) (M_state (operand1 exp) s)))))
+      ((eq? (operator stmt) '!) (M_boolean (not (M_boolean (operand1 exp) s)))))))
 
 ; M_boolean takes in a conditional statement and a state and returns true if the statement is true, and false otherwhise
 (define M_boolean
@@ -68,8 +82,8 @@
       ((lookup)))))
 
 ; M_assign takes a varriable value and state and returns the state with the varriable is assigned the value given 
-(define M_assign
-  (lambda 
+;(define M_assign
+  ;(lambda ))
 
 ;=================================================
 ; Abstractions
@@ -82,6 +96,14 @@
       ((null? stmt) (error 'thereIsNoStatement))
       ((list? stmt) (car stmt))
       (else (error 'invalidStatement)))))
+
+(define operand1
+  (lambda (math_stmt)
+    (cadr math_stmt)))
+
+(define operand2
+  (lambda (math_stmt)
+    (caddr math_stmt)))
 
 ; condition: take in an if or while statement containing a condition and returns the condition
 (define condition
